a025351f61cb8789a9def5c0e9244644c914db46
Lara Sanders <larasanders@gmail.com>
2019-07-05 18:13:21 +0000

    Automatically backed up by Learn

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -22,6 +22,8 @@ function booksReducer(state = [], action) {
   }
 }
 
+import uuid from "uuid";
+
 function authorsReducer(state = [], action) {
   let idx;
   switch (action.type) {
@@ -31,6 +33,16 @@ function authorsReducer(state = [], action) {
     case "REMOVE_AUTHOR":
       idx = state.indexOf(action.id);
       return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    case "ADD_BOOK":
+      let existingAuthor = state.filter(
+        author => author.authorName === action.book.authorName
+      );
+      if (existingAuthor.length > 0) {
+        return state;
+      } else {
+        return [...state, { authorName: action.book.authorName, id: uuid() }];
+      }
 
     default:
       return state;
